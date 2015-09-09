cf51170893399779d3f61ded55d5212a73aab23a
AlanYen <alanyen.mobile@gmail.com>
2015-09-09 16:05:28 +0800

    Init Commit

--- DemoObjcTryCatch/ViewController.m
+++ DemoObjcTryCatch/ViewController.m
@@ -24,4 +24,52 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)onTest1ButtonPressed:(id)sender {
+    
+    //
+    // 測試 NSRangeException
+    //
+    NSArray *array = @[];
+    @try {
+        NSString *firstObject = [array objectAtIndex:1];
+        NSLog(@"firstObject: %@", firstObject);
+    }
+    @catch (NSException *exception) {
+        NSLog(@"exception name is %@ (reason is %@)", exception.name, exception.reason);
+    }
+    @finally {
+        NSLog(@"finally 執行了..");
+    }
+    NSLog(@"外面執行了..");
+}
+
+- (IBAction)onTest2ButtonPressed:(id)sender {
+    
+    //
+    // 測試自定義 exception
+    //
+    @try {
+        [self testException];
+    }
+    @catch (NSException *exception) {
+        NSLog(@"exception name is %@ (reason is %@)", exception.name, exception.reason);
+    }
+    @finally {
+        NSLog(@"finally 執行了..");
+    }
+    NSLog(@"外面執行了..");
+}
+
+- (void)testException {
+    
+    NSException *excetpion =
+    [NSException exceptionWithName:@"MyException"
+                            reason:@"test exception"
+                          userInfo:nil];
+    @throw excetpion;
+    
+    //或者這樣
+    //[NSException raise:@"MyException" format:@"test exception"];
+}
+
 @end
